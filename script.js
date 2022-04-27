12ee02bf22f3d56711e06ff4ea0e2d74faa7b129
andrewinformatique <73597646+andrewinformatique@users.noreply.github.com>
2022-04-27 19:09:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ $(function () {
       "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
       "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
       "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3",
-      "http://45.58.97.188:8000/listen.mp3"
+      ""http://45.58.97.188:8000/listen.mp3"
+      
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
